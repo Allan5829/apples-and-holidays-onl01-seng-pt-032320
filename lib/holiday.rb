79737c360bf89a25f3a5d1176a320bab681ad7e2
Allan Farca <allan.farca@gmail.com>
2020-04-05 03:35:28 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -68,8 +68,7 @@ def all_supplies_in_holidays(holiday_hash)
   #   Fourth Of July: Fireworks, BBQ
   # etc.
     holiday_hash.each do |season, holiday|
-      season.to_s 
-      season.capitalize
+      season.to_s.capitalize
       puts "#{season}:"
       
       
