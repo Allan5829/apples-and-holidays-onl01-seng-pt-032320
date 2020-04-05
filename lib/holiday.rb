a42d49d386f3caf79a6bced9d36765240f222b7e
Allan Farca <allan.farca@gmail.com>
2020-04-05 02:53:10 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -56,9 +56,7 @@ def all_winter_holiday_supplies(holiday_hash)
   supply_array = []
   holiday_hash.each do |season, holiday|
     if season == :winter
-      holiday.each do |supplies|
-        supply_array << supplies
-      end
+      supply_array << holiday.values.flatten
     end
   end
   supply_array
